75ae3113737c135b28bae4efc1b709a50dee97c3
Marina Hoffmann <marina.hoffmann@aurum.com.br>
2024-09-04 10:19:00 -0300

    feat(ALL) use code_extractor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.file import read_file, write_to_file
 from utils.performance import timelapse
+from code_extractor.main import get_relevant_code
 
 from datetime import datetime
 import time
@@ -9,8 +10,18 @@ from langchain_ollama.llms import OllamaLLM
 
 start_time = time.time()
 
-code = read_file("./code_to_document.py")
+try:
+    get_relevant_code(
+        repository_path='/home/marihoffmann/projetos/hermes',
+        endpoint_file_path='/hermes/api/v3/v3.py',
+        route='/follow_group',
+        method='POST'
+    )
+except TypeError as e:
+    print(f'Houve um erro na extração do código relevante para este endpoint: {e}')
+    raise
 
+code = read_file("./combined_code_output.py")
 template = read_file("./prompt_software_engineer.txt")
 
 prompt = ChatPromptTemplate.from_template(template)
